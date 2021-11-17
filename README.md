9f5b5ad312cadc2d9ff0ec041d8a1cf121a5fcf6
Terekhin <nick.terekhin@gmail.com>
2021-11-17 04:35:41 -0600

    (FrontEnd) - added backend function to manage tickets. improved frontend sorting

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # Trello
+### Requirements
+PHP 7.3 
+Mysql
+NodeJs
+WebServer (Apache/Nginx)
+## installation
+clone repository to the WebServer folder
+then use:
+<br>npm install
+<br>configure webServer to control [project_path]/public folder.
+---
+## Front-end
+to rebuilt front-end run:
+<br>npm run dev
+
+
+
+
+
 
--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,8 @@ const webpack = require('webpack');
 const path = require('path');
 const _root = path.resolve(__dirname,'../trello');
 
-const NODE_ENV = process.env.NODE_ENV || 'development';
-       console.log(NODE_ENV);
+
+       
 function root(args){
     args = Array.prototype.slice.call(arguments,0);
     return path.join.apply(path, [_root].concat(args));
@@ -19,57 +19,60 @@ const PATHS = {
     srcAssets:`${srcFolder}/assets`,
     context:`${srcFolder}/context`,
 };
-const webPackConfig = {
-    mode:NODE_ENV,
-    entry:{
-        main:[`${PATHS.src}/index.jsx`],
-    },
-    output:{
-        path: `${PATHS.dist}`,
-        publicPath:'/',
-        filename:'[name].js',
-        chunkFilename:'[name].js'
-    },
-    resolve:{
-        modules: ['node_modules'],
-        extensions:['.tsx','.ts','.js','.jsx']
-    },
-    resolveLoader: {
-        modules: ['node_modules'],
-    },
-  
-    module: {
-        rules: [
-            {
-                test:/\.css$/,
-                use:[
-                     "style-loader",
-                    {loader:"css-loader",options:{import:true}},
-                ],
-                exclude: /node_modules/,
-                include:PATHS.src
-            },
-            {
-                test: /\.(ts|js)x?$/,
-                include:PATHS.src,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader",
-                    options: {
-                        presets: [
-                            "@babel/preset-env",
-                            "@babel/preset-react",
-                            "@babel/preset-typescript",
-                        ],
-                        plugins: [
-                            ["@babel/transform-runtime"]
-                        ]
+const webPackConfig =(env)=> {
+    
+    const NODE_ENV = env.NODE_ENV || 'development';
+    return {
+        mode: NODE_ENV,
+        entry: {
+            main: [`${PATHS.src}/index.jsx`],
+        },
+        output: {
+            path: `${PATHS.dist}`,
+            publicPath: '/',
+            filename: '[name].js',
+            chunkFilename: '[name].js'
+        },
+        resolve: {
+            modules: ['node_modules'],
+            extensions: ['.tsx', '.ts', '.js', '.jsx']
+        },
+        resolveLoader: {
+            modules: ['node_modules'],
+        },
+
+        module: {
+            rules: [
+                {
+                    test: /\.css$/,
+                    use: [
+                        "style-loader",
+                        {loader: "css-loader", options: {import: true}},
+                    ],
+                    exclude: /node_modules/,
+                    include: PATHS.src
+                },
+                {
+                    test: /\.(ts|js)x?$/,
+                    include: PATHS.src,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: "babel-loader",
+                        options: {
+                            presets: [
+                                "@babel/preset-env",
+                                "@babel/preset-react",
+                                "@babel/preset-typescript",
+                            ],
+                            plugins: [
+                                ["@babel/transform-runtime"]
+                            ]
+                        },
                     },
                 },
-            },
-        ],
-    },
-
+            ],
+        },
+    }
 }
 
 module.exports = webPackConfig;
